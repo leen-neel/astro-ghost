c4068816773c674542cfe49a3a2ed380c8cae0fe
leen-neel <and24903@gmail.com>
2022-12-29 12:59:07 +0530

    initializedapi

---
+++ src/lib/api.js
@@ -0,0 +1,18 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+// Create API instance with site credentials
+const api = new GhostContentAPI({
+    url: 'http://localhost:2368',
+    key: 'ccce3cdb9914f77bd40fa90efa',
+    version: 'v5.0',
+});
+
+export async function getPosts() {
+    return await api.posts
+        .browse({
+            limit: 'all',
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
